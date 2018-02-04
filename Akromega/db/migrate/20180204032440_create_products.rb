f4fa58461ad341ba9b69f930af846e0879b3f208
bian2510 <bian251091@gmail.com>
2018-02-04 18:22:00 -0500

    Imagenes de productos

--- Akromega/db/migrate/20180204032440_create_products.rb
+++ Akromega/db/migrate/20180204032440_create_products.rb
@@ -3,7 +3,6 @@ class CreateProducts < ActiveRecord::Migration[5.1]
     create_table :products do |t|
       t.string :title
       t.string :categorie
-
       t.timestamps
     end
   end
--- Akromega/db/schema.rb
+++ Akromega/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204032440) do
+ActiveRecord::Schema.define(version: 20180204214133) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "categorie"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
